6cfa348c3b023d161dab7ca74b37f450f983dff7
metzgermeist <javiermas.unt@gmail.com>
2024-09-13 11:10:00 -0300

    Descripción de los cambios en TP3

--- obj/Debug/net8.0/tl2-tp1-2024-metzgermeist.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp1-2024-metzgermeist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp1-2024-metzgermeist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f9de4f87c545c555c7634e3fd80634ad5cd96d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d658543b5db46512cfee8dd4e2c551e7d961ccef")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp1-2024-metzgermeist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp1-2024-metzgermeist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
